c144b5b12b5a3245cf580bf26d01bb56ff3714d6
Sohil Jivani <sohil.jivani@mykenzie.snhu.edu>
2024-04-22 15:42:37 -0400

    first commit

---
+++ src/components/header.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import { Nav, Navbar, Container } from "react-bootstrap";
+import { Link } from 'react-router-dom';
+import "./Header.css";
+
+function header () {
+
+    return(
+        <Navbar expand="lg" className="header">
+
+        </Navbar>
+    )
+    
+}
